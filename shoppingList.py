73339467cc0b9a7ae075bfd9e25f57dba2102cb1
Batman <anthony.chalabis01@gmail.com>
2022-06-21 11:18:53 -0500

    Polishing

--- shoppingList.py
+++ shoppingList.py
@@ -1,20 +1,24 @@
 shoppingCart = []
 
+
 def showHelp():
     print("What should we pick up at the store?\nEnter 'DONE' to stop adding items\nEnter 'HELP' to get help.\nEnter 'SHOW' to see your list.\nEnter 'CLEAR' to clear your list.\nEnter 'REMOVE' to remove an item.")
+
 
 def add_to_list(item):
     item = item.title()
     shoppingCart.append(item)
     print(f'Added {item}.')
 
+
 def showList():
-    print('\nSo far you have:')
+    print('Your list:')
     for item in shoppingCart:
         if item == False:
             print("Nothing to see here...")
         else:
             print(item.title())
+
 
 def clearList():
     clear = input("Are you sure? [Y/N] ")
@@ -26,8 +30,10 @@ def clearList():
         cancel = print("Action cancelled.")
         return cancel
 
+
 def removeItem():
-    removedItem = input("What item do you want to remove from your list? ").title()
+    removedItem = input(
+        "What item do you want to remove from your list? ").title()
     try:
         if removedItem not in shoppingCart:
             raise ValueError(f'{removedItem.title()} is not on the list.')
@@ -39,12 +45,14 @@ def removeItem():
 
 
 showHelp()
+
+
 def shoppingList():
     while True:
         new_item = input("Add Item: ")
         if new_item == 'DONE':
             break
-        elif new_item =='HELP':
+        elif new_item == 'HELP':
             showHelp()
             continue
         elif new_item == 'SHOW':
@@ -58,4 +66,6 @@ def shoppingList():
             continue
         add_to_list(new_item)
     showList()
+
+
 shoppingList()
